5d864f65e25e8630ad0c8d0820499c4a999ad078
VarHug <490641570@qq.com>
2017-12-14 17:24:13 +0800

    Add 058

---
+++ 058-Length of Last Word.js
@@ -0,0 +1,36 @@
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function lengthOfLastWord(s) {
+    let res = 0, right = s.length - 1;
+    while (right >= 0 && s[right] === ' ') {
+        right--;
+    }
+    while (right >= 0 && s[right] !== ' ') {
+        right--;
+        res++;
+    }
+    return res;
+}
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function lengthOfLastWord(s) {
+    s = s.trim();
+    return s.length - s.lastIndexOf(' ') - 1;
+}
+
