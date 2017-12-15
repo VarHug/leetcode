f983e8ffd5605a634be7e4f34baa476cd39e2cee
VarHug <490641570@qq.com>
2017-12-15 18:51:16 +0800

    Add 066

--- 058-Length of Last Word.js
+++ 058-Length of Last Word.js
@@ -1,13 +1,13 @@
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
 
-If the last word does not exist, return 0.
+// If the last word does not exist, return 0.
 
-Note: A word is defined as a character sequence consists of non-space characters only.
+// Note: A word is defined as a character sequence consists of non-space characters only.
 
-Example:
+// Example:
 
-Input: "Hello World"
-Output: 5
+// Input: "Hello World"
+// Output: 5
 
 /**
  * @param {string} s
