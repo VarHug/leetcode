c8e65d746e8e9ae0ecfff232e81ceab000243f35
VarHug <490641570@qq.com>
2018-07-09 20:20:57 +0800

    Add 072

---
+++ 072-Edit Distance.py
@@ -0,0 +1,47 @@
+# Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+# You have the following 3 operations permitted on a word:
+
+# Insert a character
+# Delete a character
+# Replace a character
+
+# Example 1:
+
+# Input: word1 = "horse", word2 = "ros"
+# Output: 3
+# Explanation: 
+# horse -> rorse (replace 'h' with 'r')
+# rorse -> rose (remove 'r')
+# rose -> ros (remove 'e')
+
+# Example 2:
+
+# Input: word1 = "intention", word2 = "execution"
+# Output: 5
+# Explanation: 
+# intention -> inention (remove 't')
+# inention -> enention (replace 'i' with 'e')
+# enention -> exention (replace 'n' with 'x')
+# exention -> exection (replace 'n' with 'c')
+# exection -> execution (insert 'u')
+
+def minDistance(word1, word2):
+  """
+  :type word1: str
+  :type word2: str
+  :rtype: int
+  """
+  m, n = len(word1) + 1, len(word2) + 1
+  dp = [[0 for i in range(n)] for i in range(m)]
+  for i in range(m):
+    dp[i][0] = i
+  for j in range(n):
+    dp[0][j] = j
+  for i in range(1, m):
+    for j in range(1, n):
+      if word1[i - 1] == word2[j - 1]:
+        dp[i][j] = dp[i - 1][j - 1]
+      else:
+        dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+  return dp[-1][-1]
