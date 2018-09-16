aeb2eb29f6aab60faaed5cd2b94c94b016af64cb
VarHug <490641570@qq.com>
2018-09-16 16:44:07 +0800

    add 083

---
+++ 083-Remove Duplicates from Sorted List.js
@@ -0,0 +1,56 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * 正常迭代
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  let temp = head;
+  while (temp !== null) {
+    while (temp.next && temp.val === temp.next.val) {
+      temp.next = temp.next.next;
+    }
+    temp = temp.next;
+  }
+  return head;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * 递归
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  head.next = deleteDuplicates(head.next);
+  return head.val === head.next.val ? head.next : head;
+};
